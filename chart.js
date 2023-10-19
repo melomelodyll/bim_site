6880261c2e17e22c15671ab08ddf14e99d663a09
melomelodyll <133353629+melomelodyll@users.noreply.github.com>
2023-10-19 15:41:02 +0800

    Initial commit

---
+++ chart.js
@@ -0,0 +1,16 @@
+var waterUsageData = {
+    labels: ['Bathing', 'Washing', 'Cooking'],
+    datasets: [{
+        data: [10, 15, 5], // Sample data for each category in gallons
+        backgroundColor: ['#36A2EB', '#FFCE56', '#FF6384'], // Colors for the pie chart segments
+    }]
+};
+
+var waterUsageBarData = {
+    labels: ['Bathing', 'Washing', 'Cooking'],
+    datasets: [{
+        label: 'Water Usage (in gallons)',
+        data: [10, 15, 5], // Sample data for each category in gallons
+        backgroundColor: '#36A2EB', // Color for the bars
+    }]
+};
